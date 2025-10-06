b927a19e496a2292a12d4d9ceef1049ebba0a2cd
AlejandroDTS404 <alejandrodts2022@gmail.com>
2025-10-06 18:02:29 +0200

    ejercicio 1 y 2 de la unidad 1

---
+++ UD.01/ejercicio1.py
@@ -0,0 +1,4 @@
+import math
+
+# Mostrar la raíz cuadrada de 25
+print(math.sqrt(25))
---
+++ UD.01/ejercicio2.py
@@ -0,0 +1,7 @@
+import random
+
+# Genera un número aleatorio entre 1 y 100
+numero_aleatorio = random.randint(1, 100)
+
+# Imprime el número generado
+print("El número aleatorio generado es:", numero_aleatorio)
